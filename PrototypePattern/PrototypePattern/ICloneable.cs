8d53438ee8f65599fd6b9eb19e1b1ec621751270
Albert Bennett <Albert.Bennett@livetiles.nyc>
2019-08-22 11:18:26 +0100

    Prototype Pattern

---
+++ PrototypePattern/PrototypePattern/ICloneable.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace PrototypePattern
+{
+    public interface ICloneable
+    {
+        string Id { get; }
+        string Name { get; set; }
+
+        ICloneable DeepCopy();
+        ICloneable ShallowCopy();
+
+        void LogInformation();
+    }
+
+    public class Sheep : ICloneable
+    {
+        public string Id { get; protected set; } = Guid.NewGuid().ToString();
+        public string Name { get; set; }
+
+        public ICloneable DeepCopy()
+        {
+            Sheep clone = (Sheep)MemberwiseClone();
+            clone.Name = string.Copy(Name);
+            clone.Id = Guid.NewGuid().ToString();
+
+            return clone;
+        }
+
+        public ICloneable ShallowCopy()
+        {
+            return (Sheep)MemberwiseClone();
+        }
+
+        public void LogInformation()
+        {
+            Console.WriteLine($"Name : {Name}");
+            Console.WriteLine($"ID : {Id}");
+            Console.WriteLine("------------------------------------------");
+        }
+    }
+}
---
+++ PrototypePattern/PrototypePattern/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace PrototypePattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ICloneable firstSheep = new Sheep
+            {
+                Name = "Dolly"
+            };
+
+            firstSheep.LogInformation();
+
+            ICloneable shallowCopy = firstSheep.ShallowCopy();
+            shallowCopy.LogInformation();
+
+            ICloneable deepCopy = firstSheep.DeepCopy();
+            deepCopy.LogInformation();
+
+            Console.ReadKey();
+        }
+    }
+}
