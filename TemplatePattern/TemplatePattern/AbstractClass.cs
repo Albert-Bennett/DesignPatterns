66b50719f15a1ead3d0cd8ec8068cb57dbcdc573
Albert Bennett <Albert.Bennett@livetiles.nyc>
2019-08-22 12:15:24 +0100

    Template method

---
+++ TemplatePattern/TemplatePattern/AbstractClass.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace TemplatePattern
+{
+    public abstract class AbstractClass
+    {
+        public void TemplateMethod()
+        {
+            DoSomething();
+            DoSomethingElse();
+        }
+
+        protected abstract void DoSomething();
+        protected abstract void DoSomethingElse();
+    }
+
+    public class SubClass : AbstractClass
+    {
+        protected override void DoSomething()
+        {
+            Console.WriteLine("Doing something");
+        }
+
+        protected override void DoSomethingElse()
+        {
+            Console.WriteLine("Doing something else");
+        }
+    }
+}
---
+++ TemplatePattern/TemplatePattern/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace TemplatePattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AbstractClass template = new SubClass();
+            template.TemplateMethod();
+
+            Console.ReadKey();
+        }
+    }
+}
